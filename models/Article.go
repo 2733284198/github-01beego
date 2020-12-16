3847a70c74d48b8d746fc53455a62edeb5eee990
manlan <2733284198@qq.com>
2020-12-16 12:37:32 +0800

    /* 	要加入xml的反射，才能解析出 xml的数据 */ type Article struct { 	Title   string `form:"title" xml:"title"` 	//Title   string `form:"title" ` 	Content string `form:"content" xml:"content"` 	//Content string `form:"content" ` }

--- models/Article.go
+++ models/Article.go
@@ -1,6 +1,11 @@
 package models
 
+/*
+	要加入xml的反射，才能解析出 xml的数据
+*/
 type Article struct {
-	Title   string `xml:"title"`
-	Content string `xml:"content"`
+	Title string `form:"title" xml:"title"`
+	//Title   string `form:"title" `
+	Content string `form:"content" xml:"content"`
+	//Content string `form:"content" `
 }
