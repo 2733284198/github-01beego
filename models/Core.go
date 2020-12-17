e2119058ba05f91a6cf36f020659a1c457f62a9c
manlan <2733284198@qq.com>
2020-12-17 23:40:53 +0800

    连接数据库root,root

--- models/Core.go
+++ models/Core.go
@@ -12,7 +12,7 @@ var err error
 
 func init() {
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
-	dsn := "user:pass@tcp(127.0.0.1:3306)/beego?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/beego?charset=utf8mb4&parseTime=True&loc=Local"
 
 	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
