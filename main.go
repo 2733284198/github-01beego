5c02eb30607511050e7d093623062f68301ca7c1
manlan <2733284198@qq.com>
2020-12-17 12:03:32 +0800

    beego.Appconfig.set beego.Appconfig.string beego.Appconfig.strings

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
-
 	//_ "bee01/controllers"
 	_ "bee01/routers"
 )
+
+func getconfig() {
+	port := beego.AppConfig.String("httpport")
+	beego.Info(port)
+
+	admins := beego.AppConfig.Strings("admin")
+	beego.Info(admins)
+
+	beego.AppConfig.Set("rediskey", "redisk1")
+
+	rediskey1 := beego.AppConfig.Strings("rediskey")
+	beego.Info(rediskey1)
+
+	fmt.Println(port)
+}
 
 func main() {
 
 	//beego.Router("/user", &controllers.MainController{})
 	//beego.Router("/food", &controllers.WmfoodController{})
 
+	getconfig()
+
 	beego.Run(":8089")
+	//beego.Run()
 
 }
