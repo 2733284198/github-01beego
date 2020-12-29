e42f5622c06d5b3454601add7a119e38e328d5c8
manlan <2733284198@qq.com>
2020-12-29 14:53:47 +0800

    1 xorm
    // where查询
    	var person1 Person
    	//engine.Where("id = ?", 1).Get(&person1)
    	engine.Where("age = ?", 2).Get(&person1)
    	fmt.Println("\n ====>person1 ", person1.ID, person1.Name, person1.Age)

--- 01other/xorm/xorm1.go
+++ 01other/xorm/xorm1.go
@@ -52,12 +52,49 @@ func initData() {
 	engine.Sync2(new(Person))
 
 	// 查询 person数据
+	/*
+		查询 person数据
+	*/
 
 	var person Person
-	engine.Id(1).Get(&person)
+	// id查询
+	result, err := engine.Id(1).Get(&person)
 
-	fmt.Println("\n ====> ", person.ID, person.Name, person.Age)
+	fmt.Println("\n ====>person ", person.ID, person.Name, person.Age)
+	fmt.Println(result, err)
 
+	// where查询
+	var person1 Person
+	//engine.Where("id = ?", 1).Get(&person1)
+	engine.Where("age = ?", 2).Get(&person1)
+	fmt.Println("\n ====>person1 ", person1.ID, person1.Name, person1.Age)
+
+	// 不写条件，返回所有数据
+	var person1all []Person
+	//engine.Where("id = ?", 1).Get(&person1)
+	//engine.Find(&person1all)
+	engine.OrderBy("age desc").Find(&person1all)
+	fmt.Println("\n ====>person1all ", person1all)
+
+	//engine.Where("age = ?", 2).Get(&person1s)
+	// Find：返回多条数据
+	var person1s []Person
+	engine.Where("age = ?", 2).Find(&person1s) //
+	//fmt.Println("\n ====>person1s  ", person1s.ID, person1s.Name, person1s.Age)
+	fmt.Println("\n ====>person1s  ", person1s)
+
+	// and
+	var person1sand []Person
+	//engine.Where("age = ?", 2).And("id = ?", 4).Find(&person1s) //
+	//engine.Where("age = ?", 2).And("id = ?", 4).Get(&person1) //
+	engine.Where("age = ?", 2).And("id = ?", 4).Find(&person1sand) //
+	fmt.Println("\n ====>person1sand  ", person1sand)
+	//fmt.Println("\n ====>person1  ", person1)
+
+	// 原生sql
+	var personSql []Person
+	engine.SQL("select * from person where age > 2 ").Find(&personSql)
+	fmt.Println("\n ====>personSql  ", personSql)
 }
 
 func getAllData() {
