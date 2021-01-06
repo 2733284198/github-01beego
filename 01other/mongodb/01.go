fc891b0ae720d4990113580092a9bcb73f92c328
manlan <2733284198@qq.com>
2021-01-06 10:15:49 +0800

    1. mgo:mongodb查找

--- 01other/mongodb/01.go
+++ 01other/mongodb/01.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	//"crypto/rand"
 	//"encoding/json"
 	//"fmt"
 	"log"
+	//"time"
+	"math/rand"
 	//"net/http"
 
 	//"goji.io"
@@ -39,9 +45,52 @@ func main() {
 
 	c := session.DB("user").C("people")
 
+	// 查找单本书
 	var people People
+	err = c.Find(bson.M{"age": 20}).One(&people)
+	if err != nil {
+		log.Println("查找isbn的书失败:", "age:20")
+	} else {
+		log.Println("查找isbn的书:", people)
+	}
 
-	_ = c.Find(bson.M{"age": 20}).One(&people)
-	log.Println("查找isbn的书失败: ", people)
+	// 修改书
+	people.Name = "manlan-manlan1"
+	people.Age = "21-31"
 
+	err = c.Update(bson.M{"age": 21}, &people)
+	if err != nil {
+		log.Println("修改书失败:", "age:21-31")
+	} else {
+		log.Println("修改书成功:")
+	}
+
+	// 删除书
+	err = c.Remove(bson.M{"age": 20})
+	if err != nil {
+		log.Println("删除书失败:", "age:20")
+	}
+
+	// 添加书
+	people.Name = "manlan-manlan1"
+	//people.Age = string(rune(time.Now().Second()))
+	//people.Age = string(rune(rand.Intn(50)))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	people.Age = fmt.Sprintf("%d", r.Intn(50))
+
+	err = c.Insert(people)
+	if err != nil {
+		log.Println("添加书失败:", "age:21-31")
+	} else {
+		log.Println("添加书成功:")
+	}
+
+	// 查找所有书籍
+	var peoples []People
+	err = c.Find(bson.M{}).All(&peoples)
+	if err != nil {
+		log.Println("查找isbn的书失败:")
+	} else {
+		log.Println("查找isbn的书: ", peoples)
+	}
 }
