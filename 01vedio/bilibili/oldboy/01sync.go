4c5f8267a3c435f4286b44e5ce30508d85a63454
manlan <2733284198@qq.com>
2021-01-18 17:53:19 +0800

    1.正则表达式 regexp正则表达式.md 2，邮箱地址： ["123213@qq.com" "qdasfsad@163.com"] 3. go-echarts 4. 限速：ratelimit， 5. socket：server, client
    == 多线程 ==
    1. sync.wait

--- 01vedio/bilibili/oldboy/01sync.go
+++ 01vedio/bilibili/oldboy/01sync.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func f1(i int) {
-	time.Sleep(time.Second * time.Duration(rand.Intn(10)))
+	defer wg.Done()
+
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
 	fmt.Println(i, " hello")
 }
 
+var wg sync.WaitGroup
+
 func main() {
 	for i := 0; i < 10; i++ {
+		wg.Add(1)
 		go f1(i)
 	}
 
-	time.Sleep(time.Second)
+	//time.Sleep(time.Second)
+	wg.Wait()
 }
